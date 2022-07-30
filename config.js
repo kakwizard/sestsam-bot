d04840eb9158a5e194d3fafe123258562922394b
DotDotDot99 <110268111+DotDotDot99@users.noreply.github.com>
2022-07-30 15:02:28 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const info = {
+    prefix: '.', //اكتب بريفكس بوتك
+    token: 'ODY5NzM1MTMxMDY0MTcyNTQ1.GFCzkI.mCvF-7eK9afotLR1jIB0eFq0uFO4jMyPEon_zg', // اكتب توكن بوتك
+    owner: '' // ايديك
+}
+
+
+module.exports = info;

d31ae4d36f90b8f0942c77464973422ff1585abf
DotDot999 <110268111+DotDot999@users.noreply.github.com>
2022-08-08 12:22:00 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const info = {
     prefix: '.', //اكتب بريفكس بوتك
-    token: 'ODY5NzM1MTMxMDY0MTcyNTQ1.GFCzkI.mCvF-7eK9afotLR1jIB0eFq0uFO4jMyPEon_zg', // اكتب توكن بوتك
+    token: 'ODY5NzM1MTMxMDY0MTcyNTQ1.GGboub.0SrmZQEP9aJLw4aDQ3-BzJFR2OQ2lPkMwOb3gk', // اكتب توكن بوتك
     owner: '' // ايديك
 }
 
